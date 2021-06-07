ead7c4cbbdb6215db70a7ea0783b1051a3e83a41
eko <pamuji.eko@act.id>
2021-06-07 23:13:45 +0700

    commit

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -84,7 +84,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.only(bottom: 10, left: 10),
                 child: Text(
-                  'Selamat datang !  ' + namalengkap ?? '',
+                  'Selamat datang !' + namalengkap ?? '',
                   style: TextStyle(
                       color: Colors.white, fontWeight: FontWeight.bold),
                 ),
